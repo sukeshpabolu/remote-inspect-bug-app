149e5dad5c8a18b6af2b0edce6a3f98a075f0414
sukeshpabolu <sukesh.pabolu@inter.ikea.com>
2026-01-22 12:47:49 +0100

    chore: enable experimental features

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,19 @@
 import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter(),
+		experimental: {
+			remoteFunctions: true
+		}
+	},
+	compilerOptions: {
+		sourcemap: process.env.NODE_ENV === 'development',
+		experimental: {
+			async: true
+		}
+	}
+};
 
 export default config;
